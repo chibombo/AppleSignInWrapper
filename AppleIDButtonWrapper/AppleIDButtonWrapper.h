da60d73d4efed5d144d0a85d31b66525b944761b
chibombo <genaro.arvizu95@gmail.com>
2020-04-05 13:48:07 -0500

    Initial Commit

---
+++ AppleIDButtonWrapper/AppleIDButtonWrapper.h
@@ -0,0 +1,19 @@
+//
+//  AppleIDButtonWrapper.h
+//  AppleIDButtonWrapper
+//
+//  Created by Luis Genaro Arvizu Vega on 05/04/20.
+//  Copyright © 2020 Luis Genaro Arvizu Vega. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppleIDButtonWrapper.
+FOUNDATION_EXPORT double AppleIDButtonWrapperVersionNumber;
+
+//! Project version string for AppleIDButtonWrapper.
+FOUNDATION_EXPORT const unsigned char AppleIDButtonWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppleIDButtonWrapper/PublicHeader.h>
+
+
