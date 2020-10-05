66b661a93470209b364f74b48b90228ea864e5b0
Genaro Arvizu <genaro@MacBooks-MacBook-Pro.local>
2020-10-04 20:46:15 -0500

    Version Supported added

--- Package.swift
+++ Package.swift
@@ -11,6 +11,10 @@ import PackageDescription
 
 let package = Package(
     name: "AppleIDButtonWrapper",
+    plataforms: [.iOS(.v13),
+                 .macOS(.v10_15),
+                 .tvOS(.v13),
+                 .watchOS(.v6)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
