8859da7eef52208995d9ffc6e744d58efb920a58
Luis Genaro Arvizu Vega <30416292+chibombo@users.noreply.github.com>
2020-10-05 13:39:07 -0500

    Unit Test Added (#2)
    * Unit Test Added
    
    * Fix Critical Code Smell
    
    Co-authored-by: Genaro Arvizu <genaro@MacBooks-MacBook-Pro.local>

---
+++ Tests/AppleIDButtonWrapperTests/AppleIDLoginDelegateSpy.swift
@@ -0,0 +1,28 @@
+//
+//  AppleIDLoginDelegateSpy.swift
+//  AppleIDButtonWrapper
+//
+//  Created by Genaro Arvizu on 05/10/20.
+//  Copyright © 2020 Luis Genaro Arvizu Vega. All rights reserved.
+//
+
+import Foundation
+@testable import AppleIDButtonWrapper
+class AppleIDLoginDelegateSpy: AppleIDLoginDelegate {
+       
+    var isSignIn: Bool = false
+    
+    var user: UserInformation!
+    var error: Error = NSError()
+    
+    func appleSignInWrapper(didComplete withError: Error) {
+        isSignIn = false
+        user = nil
+        error = withError
+    }
+    
+    func appleSignInWrapper(didComplete withUser: UserInformation) {
+        isSignIn = true
+        user = withUser
+    }
+}
---
+++ Tests/AppleIDButtonWrapperTests/AppleSignInWrapperTests.swift
@@ -0,0 +1,58 @@
+//
+//  AppleSignInWrapperTests.swift
+//  AppleIDButtonWrapperTests
+//
+//  Created by Genaro Arvizu on 05/10/20.
+//  Copyright © 2020 Luis Genaro Arvizu Vega. All rights reserved.
+//
+
+import XCTest
+import AuthenticationServices
+@testable import AppleIDButtonWrapper
+class AppleSignInWrapperTests: XCTestCase {
+
+    var wrapper: AppleSignInWrapper!
+    var spy: AppleIDLoginDelegateSpy!
+    
+    override func setUpWithError() throws {
+        wrapper = AppleSignInWrapper()
+        spy = AppleIDLoginDelegateSpy()
+        wrapper.delegate = spy
+    }
+
+    override func tearDownWithError() throws {
+        wrapper = nil
+        spy = nil
+    }
+
+    func testDelegate_whenFail_signIn() throws {
+        wrapper.view = UIView()
+        let error: NSError = NSError(domain: "Local Error",
+                                     code: 0,
+                                     userInfo: [NSLocalizedDescriptionKey: "Local Error"])
+        wrapper.delegate?.appleSignInWrapper(didComplete: error)
+        
+        XCTAssert(spy.error.localizedDescription == error.localizedDescription, "Different Error")
+    }
+    
+    func testDelegate_WhenUser_SignIn() throws {
+        wrapper = AppleSignInWrapper(view: UIView())
+        wrapper.delegate = spy
+        let identifier: String = UUID().uuidString
+        let firstName: String = "Fulano"
+        let lastName: String = "Perengano"
+        let email: String = "your@email.com"
+        let token: String = "Token"
+        
+        let model: UserInformation = UserInformation(userIdentifier: identifier,
+                                                     firstName: firstName,
+                                                     lastName: lastName,
+                                                     email: email,
+                                                     token: token)
+        
+        wrapper.delegate?.appleSignInWrapper(didComplete: model)
+        
+        XCTAssert(spy.user.userIdentifier == model.userIdentifier, "Different User")
+    }
+
+}
